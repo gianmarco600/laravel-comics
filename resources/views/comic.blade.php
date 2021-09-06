0ce84d26ba763d9fb6801f97ac67a44d60efa9c0
gianmarco <gianmarco.palombo600@gmail.com>
2021-09-06 21:45:33 +0200

    fine

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -1,27 +1,32 @@
 @extends('layouts.app')
 
-@section('title', 'comics')
+@section('title', 'comic')
     
 @section('main_content')
 <?php 
 
-if ($_GET["key"] != ''){
-    $key = $_GET["key"];
-};
+
+$key = $_GET["key"];
+
 
 ?> 
-<div class="container">
-    <h2 class="my-20" >{{$comics[$key]['title']}}</h2>
+<div class="container clearfix white">
+    <div class="card_info">
+       <h2 class="" >{{$comics[$key]['title']}}</h2>
+        <div class="shadow">
+            <img class="thumb my-20" src="{{$comics[$key]['thumb']}}" alt="">
+        </div>
+        
+        <p>{{$comics[$key]['description']}}</p>
+        <div class="prezzo my-20">{{$comics[$key]['price']}}</div>
+        <p>a cura di:</p>
+        <ul class="my-20">
+            @foreach ($comics[$key]['artists'] as $person)
+                <li>{{$person}}</li>
+            @endforeach
+        </ul> 
+    </div>
     
-    <img class="my-20" src="{{$comics[$key]['thumb']}}" alt="">
-    <p>{{$comics[$key]['description']}}</p>
-    <div class="prezzo my-20">{{$comics[$key]['price']}}</div>
-    <p>a cura di:</p>
-    <ul class="my-20">
-        @foreach ($comics[$key]['artists'] as $person)
-            <li>{{$person}}</li>
-        @endforeach
-    </ul>
 </div>
     
     
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,18 +4,21 @@
 @section('title', 'DC comics')
     
 @section('main_content')
-<div class="container">
-    <div class="row wrap">
+<div class="container rel">
+    <div class="banner">
+        <h4>current series</h4>
+    </div>
+    <div class="row wrap ">
         @foreach ($comics as $key => $comic)
-            <div class="col-2 my-20 p-20">
-                <div class="card">
+            <div class="col-2 my-30 p-20">
+                <a href="{{route('comic')}}?key={{$key}}" class="card">
                     
                     
                     <div class="bgimg">
                         <img src="{{$comic['thumb']}}" alt="">
                     </div>
-                    <div class="etichetta"><a href="{{route('comic')}}?key={{$key}}">{{$comic['title']}}</a></div>
-                </div>
+                    <div class="etichetta">{{$comic['title']}}</div>
+                </a>
                 
             </div>
         @endforeach
